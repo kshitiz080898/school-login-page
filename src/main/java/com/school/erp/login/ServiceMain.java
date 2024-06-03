7691920a75d98c769f39ea9b2f2afc10a26a827d
Aditya Kompalli <adityakompalli@gmail.com>
2024-06-03 23:25:34 +0530

    Init commit

---
+++ src/main/java/com/school/erp/login/ServiceMain.java
@@ -0,0 +1,13 @@
+package com.school.erp.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceMain.class, args);
+    }
+
+}
