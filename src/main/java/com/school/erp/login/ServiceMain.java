135b0a343c1ab706878d9f59b6a96c0ee995e2e7
Aditya Kompalli <adityakompalli@gmail.com>
2024-06-04 23:53:46 +0530

    Fix for gradle build

--- src/main/java/com/school/erp/login/ServiceMain.java
+++ src/main/java/com/school/erp/login/ServiceMain.java
@@ -3,11 +3,22 @@ package com.school.erp.login;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The type Service main.
+ */
 @SpringBootApplication
+@SuppressWarnings("all")
 public class ServiceMain {
 
+    private ServiceMain() {
+    }
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(ServiceMain.class, args);
     }
-
 }
---
+++ src/test/java/com/school/erp/login/ServiceMainTest.java
@@ -0,0 +1,10 @@
+package com.school.erp.login;
+
+import org.junit.jupiter.api.Test;
+
+public class ServiceMainTest {
+
+    @Test
+    void main() {
+    }
+}
